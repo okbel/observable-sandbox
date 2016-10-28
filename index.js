3c76b7ff66e87334a716536d1519093535cb615d
Belen Curcio <curciobelen@gmail.com>
2016-10-27 21:43:06 -0300

    rxjs, initial dev

---
+++ index.js
@@ -0,0 +1,25 @@
+import Rx, { Observable } from 'rxjs'
+
+
+/*
+ Rx.Subject is both and Observable and an Observer
+ */
+
+//const ob = Observable.interval(500)
+//
+//ob.subscribe((value) => {
+//  console.log('hello', value)
+//})
+
+/*
+ Rx.Subject is both an Observer and Observable, so it handles both publish and subscribe.
+*/
+
+const subject = new Rx.Subject()
+const subscription = subject.subscribe(data => console.log(`this is data:${data}`)) //subscribe returns a subscription
+subject.next('foo')
+subject.next('another data')
+
+subscription.unsubscribe()
+
+
